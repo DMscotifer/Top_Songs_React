e8b5460e4c145f241d65d493c5739250c568151a
Scott Stevenson <scotifer92@googlemail.com>
2018-06-27 08:02:04 +0100

    Adds url

--- topsongs/src/containers/topSongs.js
+++ topsongs/src/containers/topSongs.js
@@ -4,9 +4,15 @@ class topSongs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      songs: [],
+      songs: []
     }
   }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"
+    fetch(url).then(res => res.json()).then(songs => this.setState({songs: songs.feed.entry}));
+  }
+
   render() {
     return (
       <div>
